fd763b3c348c0813b8be42b94e2cb67e6ae84ab3
Laura <laura.weihl@icloud.com>
2020-01-11 14:15:34 +0100

    added more activation functions

--- chap03_nns.py
+++ chap03_nns.py
@@ -1,5 +1,4 @@
 import torch
-import torch.nn as nn
 import matplotlib.pyplot as plt 
 from datetime import datetime
 import seaborn as sns
@@ -7,8 +6,10 @@ import seaborn as sns
 
 plot_dir = "plots/"
 
+sns.set_style("whitegrid")
 
-class Perceptron(nn.Module):
+
+class Perceptron(torch.nn.Module):
 	""" A perceptron is one linear layer """
 	def __init__(self, input_dim: int):
 		""" 
@@ -16,7 +17,7 @@ class Perceptron(nn.Module):
 			input_dim(int): size of input features
 		"""
 		super(Perceptron, self).__init__()
-		self.fc1 = nn.Linear(input_dim, 1)
+		self.fc1 = torch.nn.Linear(input_dim, 1)
 
 	def forward(self, x_in: torch.Tensor):
 		""" The forward pass of the perceptron
@@ -35,31 +36,62 @@ def describe(x):
 
 def ex3_2():
 	# plotting the sigmoid function
-	x = torch.range(-5, 5, 0.1)
+	x = torch.arange(-5, 5, 0.1)
 	y = torch.sigmoid(x)
-		
-	f, ax = plt.subplots()
-	sns.scatterplot(x=x, y=y)
-	ax.set(xlabel='x', ylabel='f(x)', title="Sigmoid function")
-	f.savefig(plot_dir + __file__[:-3] + "_3_2.pdf")
+	
+	myplot(x, y, "3_2", "Sigmoid function")
+
 
 def ex3_3():
 	# plotting the tanh function
-	x = torch.range(-5, 5, 0.1)
+	x = torch.arange(-5, 5, 0.1)
 	y = torch.tanh(x)
 		
+	myplot(x, y, "3_3", "Tanh function")
+
+
+def ex3_4():
+	# plotting the relu function
+	x = torch.arange(-5, 5, 0.1)
+	relu = torch.nn.ReLU()
+	y = relu(x)
+		
+	myplot(x, y, "3_4", "ReLU function")
+
+
+def ex3_5():
+	# plotting the prelu function
+	x = torch.arange(-5, 5, 0.1)
+	prelu = torch.nn.PReLU(num_parameters=1)
+	y = prelu(x)
+		
+	myplot(x, y, "3_5", "PReLU function")
+
+
+def ex3_6():
+	# plotting the softmax function
+	x = torch.arange(-5, 5, 0.1).unsqueeze(0)
+	softmax = torch.nn.Softmax(dim=1)
+	y = softmax(x)
+	print(x.shape, y.shape)
+		
+	myplot(x.squeeze(), y.squeeze(), "3_6", "Softmax function")
+
+
+def myplot(x, y, name, title):
 	f, ax = plt.subplots()
-	sns.scatterplot(x=x, y=y)
-	ax.set(xlabel='x', ylabel='f(x)', title="Tanh function")
-	f.savefig(plot_dir + __file__[:-3] + "_3_3.pdf")
+	sns.scatterplot(x=x.detach().numpy(), y=y.detach().numpy())
+	ax.set(xlabel='x', ylabel='f(x)', title=title)
+	f.savefig(plot_dir + __file__[:-3] + "_" + name + ".pdf")
 
 
 def main():
 	print(datetime.now(), 'Started')
 
-	ex3_3()
+	ex3_6()
 
 	print(datetime.now(), 'Finished')
+
 
 if __name__ == "__main__":
 	main()
